a6d71bc003dfce9ceb48a007bd47db65f740cf50
Spatocode <impulseofficial069@gmail.com>
2018-11-01 15:43:02 +0100

    Bug fixes/readability improvements

--- scripts/controller.js
+++ scripts/controller.js
@@ -1,28 +1,28 @@
-var welcome = $('.welcome')
-var game = $('#game')
-var question = $('.question')
-var options = $('.options')
-var warning = $('.warning')
-var yes = $(".yes")
-var no = $(".no")
-var right_wrapper = $('.right-wrapper')
-var right = $('#right')
-var wrong_wrapper = $('.wrong-wrapper')
-var wrong = $('#wrong')
-var chat = $('#chat')
-var chat_wrapper = $('.chat-wrapper')
-window.stageLen = 1;
-var selected
-var taken = [];
-var c = 2
-window.gameLen = $(".stages button").length
-window.pLen = gameLen
-var amount = $(".stages button").eq(pLen-1).html();
-window.rand = Math.floor(Math.random()*3)
-var audience = true
-var call_a_friend = true
-var fifty_fifty = true
-var play = true
+let welcome = $('.welcome')
+let game = $('#game')
+let question = $('.question')
+let options = $('.options')
+let warning = $('.warning')
+let yes = $(".yes")
+let no = $(".no")
+let right_wrapper = $('.right-wrapper')
+let right = $('#right')
+let wrong_wrapper = $('.wrong-wrapper')
+let wrong = $('#wrong')
+let chat = $('#chat')
+let chat_wrapper = $('.chat-wrapper')
+let stageLen = 1;
+let selected
+let taken = [];
+let c = 2
+let gameLength = $(".stages button").length
+let pLen = gameLength
+let amount = $(".stages button").eq(pLen-1).html();
+let rand = Math.floor(Math.random()*3)
+let audience = true
+let call_a_friend = true
+let fifty_fifty = true
+let play = true
 
 function random(){
     try{
@@ -38,7 +38,7 @@ function random(){
     }
 }
 
-var app = {
+let app = {
     start_game: function start_game(){
         rand = random()
     
@@ -46,7 +46,8 @@ var app = {
                 game.fadeIn(500)
             });
     
-            setTimeout(function(){
+            setTimeout(
+                function(){
                 if(stageLen < 5) {
                     question.html(stages.stage1.que[rand])
                     options.html(stages.stage1.op[rand])
@@ -57,7 +58,8 @@ var app = {
                     question.html(stages.stage3.que[rand])
                     options.html(stages.stage3.op[rand])
                 }
-            },500)
+            },
+            500)
     },
     check: function check(clicked){
         if(play){
@@ -87,28 +89,41 @@ var app = {
             $(".current").removeClass("current");
             $(".stages button").eq(pLen-1).addClass("current");
             $(".score").html("Score: " + amount)
-            gameLen++;
+            gameLength++;
     
-            setTimeout(function(){
-                app.start_game()
+            setTimeout(() => {
+                this.start_game()
                 call_a_friend = true, audience = true, fifty_fifty = true, play = true
-            },4500)
+            },
+            4500)
         }
         else{
             setTimeout(function(){right.html("You are right!!!")},500)
             setTimeout(function(){right.html("Congratulations!!! You are now a millionaire")},1800)
-            setTimeout(function(){
-                right_wrapper.fadeOut(800, function() {
-                    right.html("")
-                    reset()
-                })}, 2300)
+            setTimeout(
+                () => {
+                    right_wrapper.fadeOut(800, () => {
+                        right.html("")
+                        this.reset()
+                    })
+                },
+                 2300)
         }
     },
     loose: function loose(){
         wrong_wrapper.fadeIn(800);
-        setTimeout(function(){wrong.html("Sorry, you are wrong!!!<br>You are leaving with " + amount)},100)
-        setTimeout(function(){wrong.fadeOut(function(){wrong.html("")})}, 1500)
-        setTimeout(function(){reset()},1500) 
+
+        setTimeout(
+            function(){wrong.html(`Sorry, you are wrong!!!<br>You are leaving with ${amount}`)}
+        ,100)
+
+        setTimeout(
+            function(){
+                wrong_wrapper.fadeOut(function(){wrong.html("")})
+            }
+        ,1500)
+        
+        setTimeout(() => { this.reset() }, 1500) 
     },
     fifty: function fifty(){
         if(fifty_fifty){
@@ -120,33 +135,60 @@ var app = {
     call: function call(){
         if(call_a_friend){
             play = false, audience = false, fifty_fifty = false
-            var friend = ["Ogochukwu","Ekene","Emeka","Chimobi","Chidera","Chisom","Iweka"]
+            let friend = ["Ogochukwu","Ekene","Emeka","Chimobi","Chidera","Chisom","Iweka"]
             randFriend = Math.floor(Math.random()*7);
-            var randOpt = ["A","B","C","D"]
+            let randOpt = ["A","B","C","D"]
             randAns = Math.floor(Math.random()*4);
-            var sure = ["100%","80%","60%","50%"]
-            var resp = ["It\'s certainly","I think it\'s","Am sure it\'s","It\'s definitely","Am not sure to choose"]
-            var randResp = Math.floor(Math.random()*5)
+            let sure = ["100%","80%","60%","50%"]
+            let resp = ["It\'s certainly","I think it\'s","Am sure it\'s","It\'s definitely","Am not sure to choose"]
+            let randResp = Math.floor(Math.random()*5)
     
-            $(".call_a_friend").attr({"src":"../images/call_a_friend2.png","onclick":""}).css("cursor","default")
-            $(".call_a_friend:hover").css("background-color","rgb(17, 17, 138)")
-            chat-wrapper.fadeIn(500,function(){game.css("opacity","0.8")});
+            $(".call").attr({"src":"../images/call2.png","onclick":""}).css("cursor","default")
+            $(".call:hover").css("background-color","rgb(17, 17, 138)")
+            chat_wrapper.fadeIn(500,function(){game.css("opacity","0.8")});
     
-            setTimeout(function(){chat.html("Call_a_friending...")},100)
-            setTimeout(function(){chat.html("Connected")},2000)
-            setTimeout(function(){chat.html("ME: Hello "+friend[randFriend] +". Am in a hot seat now and i need the answer to this question.<br>"+$(".quesCon").html())},3800)
-            setTimeout(function(){chat.html(friend[randFriend]+": Thinking...")},7000)
-            setTimeout(function(){chat.html(friend[randFriend]+": "+resp[randResp]+", "+randOpt[randAns]+".")},10000)
-            setTimeout(function(){chat.html("ME: How sure are you?")},13000)
-            setTimeout(function(){chat.html(friend[randFriend]+": "+sure[randAns]+" sure.")},16000)
-            setTimeout(function(){
-                chat-wrapper.fadeOut(800, function() {
-                    chat.html("")
-                    game.css("opacity", "1")
-                    play = true
-                    audience = true
-                    fifty_fifty = true
-                })}, 18000)
+            setTimeout(() => {
+                chat.html("Calling...")
+            },100)
+
+            setTimeout(() => {
+                chat.html("Connected")
+            },2000)
+
+            setTimeout(
+                function(){
+                chat.html(`ME: Hello ${friend[randFriend]}. Am in a hot seat now and i need the answer to this question.<br>${question.html()}`)
+            }
+            ,3800)
+            
+            setTimeout(
+                function(){chat.html(`${friend[randFriend]}: Thinking...`)
+            }
+            ,7000)
+
+            setTimeout(
+                function(){chat.html(`${friend[randFriend]} ${resp[randResp]}, ${randOpt[randAns]}.`)}
+            ,10000)
+
+            setTimeout(
+                function(){chat.html("ME: How sure are you?")}
+            ,13000)
+
+            setTimeout(
+                function(){chat.html(`${friend[randFriend]}: ${sure[randAns]} sure.`)}
+            ,16000)
+            
+            setTimeout(
+                function(){
+                    chat_wrapper.fadeOut(800, function() {
+                        chat.html("")
+                        game.css("opacity", "1")
+                        play = true
+                        audience = true
+                        fifty_fifty = true
+                    })
+            }
+                , 18000)
             }
     },
     audience: function aud(){
@@ -155,25 +197,38 @@ var app = {
     
             $(".aud").attr({"src":"../images/aud2.png","onclick":""}).css("cursor","default")
             $(".aud:hover").css("background-color","rgb(17, 17, 138)")
-            $(".chat-wrapper").fadeIn(500,function(){$("#game").css("opacity","0.8")})
-            setTimeout(function(){chat.html("Throwing question to audience...")},100)
-            setTimeout(function(){chat.html("Audience thinking...")},1200)
-            setTimeout(function(){
+            $(".chat_wrapper").fadeIn(500,function(){$("#game").css("opacity","0.8")})
+            
+            setTimeout(
+                function(){chat.html("Throwing question to audience...")}
+            ,100)
+
+            setTimeout(
+                function(){chat.html("Audience thinking...")}
+            ,1200)
+
+            setTimeout(
+                function(){
                 audience_wrapper.fadeIn()
-                play = true, call_a_friend = true, fifty_fifty = true},2700)
-    
+                play = true, call_a_friend = true, fifty_fifty = true
+                }
+            ,2700)
         }
     },
     reset: function reset(){
         $("#game").fadeOut()
-        setTimeout(function(){$(".welcome").fadeIn(600)},1000)
+
+        setTimeout(
+            function(){$(".welcome").fadeIn(600)}
+        ,1000)
     
         call_a_friend = true, audience = true, fifty_fifty = true, play = true
         window.stageLen = 1;
         taken = [];
         c = 2
-        window.gameLen = $(".stages button").length
-        window.pLen = gameLen
+
+        gameLength = $(".stages button").length
+        pLen = gameLength
         amount = $(".stages button").eq(pLen-1).html();
         
         $("img:hover").css("background-color","rgb(250, 121, 0) !important")
@@ -184,17 +239,21 @@ var app = {
 
 no.click(function() {
     call_a_friend = true, audience = true, fifty_fifty = true, play = true
-    warning.fadeOut(function(){
-        game.css("opacity", "1")
-    })
+    warning.fadeOut(
+        function(){
+            game.css("opacity", "1")
+        }
+    )
 });
 
 yes.click(function(){
-    warning.fadeOut(500,function(){
-        game.css("opacity", "1")
-        var correct = 2;
-        (selected == correct) ? app.won() : app.loose()
-    })
+    warning.fadeOut(500,
+        function(){
+            game.css("opacity", "1")
+            let correct = 2;
+            (selected == correct) ? app.won() : app.loose()
+        }
+    )
 })
 
 const stages = {
