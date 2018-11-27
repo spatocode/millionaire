a9f0ff2be211a6d19e2496b1ca115cd59241fade
Spatocode <impulseofficial069@gmail.com>
2018-11-27 08:49:19 +0100

    Included more data questions in app

--- scripts/data.js
+++ scripts/data.js
@@ -62,5 +62,133 @@ var stages = {
               "ans": "D"
             }
         ]
-    }
+    },
+    stage2: {
+      data: [
+          {
+            "question": "What is the full meaning of PHP?",
+            "options": [
+              "A: Hypertext Preprocessor",
+              "B: Pre Hypertext Processor",
+              "C: Page High Power",
+              "D: Power High Page",
+            ],
+            "ans": "A"
+          },
+          {
+            "question": "Which of these is not a JavaScript library?",
+            "options": [
+              "A: jQuery",
+              "B: Angular",
+              "C: React",
+              "D: Ember",
+           ],
+           "ans": "B"
+          },
+          {
+            "question": "Which is a CSS framework?",
+            "options": [
+              "A: Popper",
+              "B: Modernizr",
+              "C: Bootstrap",
+              "D: Sass",
+            ],
+            "ans": "C"
+          },
+          {
+            "question": "Which language is synonymous to closure?",
+            "options": [
+              "A: Python",
+              "B: Hash",
+              "C: C++",
+              "D: JavaScript",
+            ],
+            "ans": "D"
+          },
+          {
+            "question": "Who is the creator of Python?",
+            "options": [
+              "A: Guido van Rossium",
+              "B: Brendan Eich",
+              "C: Google Inc",
+              "D: Dan Abrahmov",
+            ],
+            "ans": "A"
+          },
+          {
+            "question": "Which of these is not a framework for building Android application?",
+            "options": [
+              "A: React Native",
+              "B: Ionic",
+              "C: Phonegap",
+              "D: Kotlin",
+            ],
+            "ans": "D"
+          }
+      ]
+  },
+  stage3: {
+    data: [
+        {
+          "question": "What is the full meaning of PHP?",
+          "options": [
+            "A: Hypertext Preprocessor",
+            "B: Pre Hypertext Processor",
+            "C: Page High Power",
+            "D: Power High Page",
+          ],
+          "ans": "A"
+        },
+        {
+          "question": "Which of these is not a JavaScript library?",
+          "options": [
+            "A: jQuery",
+            "B: Angular",
+            "C: React",
+            "D: Ember",
+         ],
+         "ans": "B"
+        },
+        {
+          "question": "Which is a CSS framework?",
+          "options": [
+            "A: Popper",
+            "B: Modernizr",
+            "C: Bootstrap",
+            "D: Sass",
+          ],
+          "ans": "C"
+        },
+        {
+          "question": "Which language is synonymous to closure?",
+          "options": [
+            "A: Python",
+            "B: Hash",
+            "C: C++",
+            "D: JavaScript",
+          ],
+          "ans": "D"
+        },
+        {
+          "question": "Who is the creator of Python?",
+          "options": [
+            "A: Guido van Rossium",
+            "B: Brendan Eich",
+            "C: Google Inc",
+            "D: Dan Abrahmov",
+          ],
+          "ans": "A"
+        },
+        {
+          "question": "Which of these is not a framework for building Android application?",
+          "options": [
+            "A: React Native",
+            "B: Ionic",
+            "C: Phonegap",
+            "D: Kotlin",
+          ],
+          "ans": "D"
+        }
+    ]
+  }
 }
